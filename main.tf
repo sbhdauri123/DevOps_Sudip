95ec163bea4be0f1ba89c5811dbd22bc2ee87e7c
sbhdauri123 <sudipbhaduri@gmail.com>
2020-02-19 14:10:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 
 provider "azurerm" {
-    subscription_id = "3e868ff3-d314-42db-b61a-009e4b8bc579"
-    client_id       = "1f32419d-c1be-4918-b6af-01171988e69a"
-    client_secret   = "25e06ebb-dccc-42c0-aa5b-91eddbb8e9c2"
-    tenant_id       = "a4481216-6ec9-4cab-a365-de0c9b2f6c9a"
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=1.44.0"
 }
 
 resource "azurerm_resource_group" "tf_azure_guide" {
